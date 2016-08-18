405f2b6ba7a41a4c31248fb1dbefb2e67f206bf5
Luan Pereira <luantomazelli@gmail.com>
2016-08-17 18:33:09 -0700

    Add files via upload

---
+++ js/openWindow.js
@@ -0,0 +1,22 @@
+/***************************
+This is JavaScript (JS), the programming language that powers the web (and this is a comment, which you can delete).
+
+To use this file, link it to your markup by placing a <script> in the <body> of your HTML file:
+
+  <body>
+    <script src="script.js"></script>
+
+replacing "script.js" with the name of this JS file.
+
+Learn more about JavaScript at
+
+https://developer.mozilla.org/en-US/Learn/JavaScript
+***************************/
+
+function openWindow() {
+  window.open("http://www.w3schools.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
+
+openWindow();
+
+
